320e3d9c14f1044a18f0869629bb73e1be237f5c
snorlon <snorlon@hotmail.com>
2015-02-21 16:51:06 -0800

    Added in graphing of points.

--- PA01/main.cpp
+++ PA01/main.cpp
@@ -13,6 +13,25 @@ int main(int argc, char** argv)
     int probabilityA = 0.5f;
     int probabilityB = 0.5f;
 
+
+
+
+    Mat image;
+
+    image = imread( "graph.png", 1 );
+
+    int width = image.cols;
+    int height = image.rows;
+
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+
+
+
+
     if(argc > 1)
     {
         //use the first parameter as a seed for replicating results
@@ -24,6 +43,9 @@ int main(int argc, char** argv)
     float mean[2] = {1.0f,1.0f};
     float stdDev[2][2] = {{1.0f,0.0f},{0.0f,1.0f}};
 
+    int intensity = 1;
+    int spread = 40;
+
     for(int i=0; i< 10000; i++)
     {
         pointSet[i][0] = 0.0f;
@@ -31,6 +53,29 @@ int main(int argc, char** argv)
         pointSet[i][2] = 0.0f;//class
 
         box_muller2d(pointSet[i], mean, stdDev);
+
+        //plot the points
+
+        int x = 500 - (pointSet[i][0] * spread);
+        int y = pointSet[i][1] * spread;
+
+        if(x >= 500)
+            x = 499;
+        if(y >= 500)
+            y = 499;
+        if(x < 0)
+            x = 0;
+        if(y < 0)
+            y = 0;
+
+        Vec3b color = image.at<Vec3b>(Point(100+x,y));
+
+        color[2] += intensity;
+
+        if(color[2] > 255)
+            color[2] = 255;
+
+        image.at<Vec3b>(x,100+y) = color; 
 
         //cout<<"("<<pointSet[i][0]<<","<<pointSet[i][1]<<")"<<endl;
     }
@@ -46,6 +91,35 @@ int main(int argc, char** argv)
 
         box_muller2d(pointSet[i+numPoints], meanb, stdDevb);
 
+        //plot the points
+
+        int x = 500 - (pointSet[i+numPoints][0] * spread);
+        int y = pointSet[i+numPoints][1] * spread;
+
+        if(x >= 500)
+            x = 499;
+        if(y >= 500)
+            y = 499;
+        if(x < 0)
+            x = 0;
+        if(y < 0)
+            y = 0;
+
+        Vec3b color = image.at<Vec3b>(Point(100+x,y));
+
+        color[1] += intensity;
+
+        if(color[1] > 255)
+            color[1] = 255;
+
+        image.at<Vec3b>(x,100+y) = color; 
+
         //cout<<"("<<pointSet[i+numPoints][0]<<","<<pointSet[i+numPoints][1]<<")"<<endl;
     }
+
+
+    namedWindow("Display Image", CV_WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+
+    waitKey(0);
 }
